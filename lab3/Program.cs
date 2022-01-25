f1a1dc24f026e101dbd6217301bf427ce3f27d76
TanyaMaximova1 <tatamurom@gmail.com>
2022-01-26 00:40:11 +0300

    lab3

---
+++ lab3/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace lab3
+{
+	[TestFixture]
+
+	public class Tests
+    {
+		[Test]
+		public void MoreTest()
+		{
+			Assert.IsTrue(new SemVersion("1.1.0") > new SemVersion("1.0.0"));
+		}
+
+		[Test]
+		public void LessTest()
+		{
+			Assert.IsTrue(new SemVersion("1.0.0") < new SemVersion("1.1.0"));
+		}
+
+		[Test]
+		public void MoreOrEqualTest()
+		{
+			Assert.IsTrue(new SemVersion("1.1.0") >= new SemVersion("1.0.0"));
+			Assert.IsTrue(new SemVersion("1.0.0") >= new SemVersion("1.0.0"));
+		}
+
+		[Test]
+		public void LessOrEqualTest()
+		{
+			Assert.IsTrue(new SemVersion("1.0.0") <= new SemVersion("1.1.0"));
+			Assert.IsTrue(new SemVersion("1.0.0") <= new SemVersion("1.0.0"));
+		}
+
+		[Test]
+		public void EqualTest()
+		{
+			Assert.IsTrue(new SemVersion("1.0.0") == new SemVersion("1.0.0"));
+			Assert.IsTrue(new SemVersion("1.1.0") == new SemVersion("1.1.0"));
+		}
+
+		[Test]
+		public void NoEqualTest()
+		{
+			Assert.IsTrue(new SemVersion("1.0.0") != new SemVersion("1.1.0"));
+			Assert.IsTrue(new SemVersion("1.1.0") != new SemVersion("1.0.0"));
+		}
+	}
+}
